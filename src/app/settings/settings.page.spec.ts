93fa78190d5013a3c664eb942a1ca2db7265ccdc
Ken Sodemann <ken@ionic.io>
2022-11-08 16:59:46 -0600

    feat(auth): allow a custom auth

--- src/app/settings/settings.page.spec.ts
+++ src/app/settings/settings.page.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { AuthenticationService } from '@app/core';
 import { createAuthenticationServiceMock } from '@app/core/testing';
@@ -9,7 +11,7 @@ import {
   oktaConfig,
 } from '@env/environment';
 import { IonicModule } from '@ionic/angular';
-import { click } from '@test/util';
+import { click, setInputValue } from '@test/util';
 import { SettingsPage } from './settings.page';
 
 describe('SettingsPage', () => {
@@ -19,7 +21,7 @@ describe('SettingsPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SettingsPage],
-      imports: [IonicModule],
+      imports: [IonicModule, FormsModule],
       providers: [
         {
           provide: AuthenticationService,
@@ -27,6 +29,11 @@ describe('SettingsPage', () => {
         },
       ],
     }).compileComponents();
+
+    const auth = TestBed.inject(AuthenticationService);
+    (auth.getBaseConfig as jasmine.Spy).and.resolveTo({
+      ...auth0Config,
+    });
 
     fixture = TestBed.createComponent(SettingsPage);
     component = fixture.componentInstance;
@@ -66,6 +73,82 @@ describe('SettingsPage', () => {
       expect(button.nativeElement.disabled).toBe(true);
       button = fixture.debugElement.query(By.css('[data-testid="use-okta"]'));
       expect(button.nativeElement.disabled).toBe(true);
+      button = fixture.debugElement.query(
+        By.css('[data-testid="use-customization"]')
+      );
+      expect(button.nativeElement.disabled).toBe(true);
+    });
+
+    describe('client ID', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="client-id-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.clientID);
+      });
+
+      it('is disabled', () => {
+        expect(input.nativeElement.disabled).toEqual(true);
+      });
+    });
+
+    describe('discovery URL', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="discovery-url-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.discoveryUrl);
+      });
+
+      it('is disabled', () => {
+        expect(input.nativeElement.disabled).toEqual(true);
+      });
+    });
+
+    describe('scope', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="scope-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.scope);
+      });
+
+      it('is disabled', () => {
+        expect(input.nativeElement.disabled).toEqual(true);
+      });
+    });
+
+    describe('audience', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="audience-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.audience);
+      });
+
+      it('is disabled', () => {
+        expect(input.nativeElement.disabled).toEqual(true);
+      });
     });
   });
 
@@ -155,6 +238,123 @@ describe('SettingsPage', () => {
         expect(authentication.setBaseConfig).toHaveBeenCalledTimes(1);
         expect(authentication.setBaseConfig).toHaveBeenCalledWith(oktaConfig);
       }));
+    });
+
+    describe('customize button', () => {
+      let button: any;
+      beforeEach(() => {
+        button = fixture.debugElement.query(
+          By.css('[data-testid="use-customization"]')
+        );
+      });
+
+      it('saves the config', waitForAsync(() => {
+        let input = fixture.debugElement.query(
+          By.css('[data-testid="client-id-input"]')
+        );
+        setInputValue(fixture, input.nativeElement, '1994-9940fks');
+        input = fixture.debugElement.query(
+          By.css('[data-testid="discovery-url-input"]')
+        );
+        setInputValue(
+          fixture,
+          input.nativeElement,
+          'https://foo.bar.disco/.well-known/sticky-buns'
+        );
+        input = fixture.debugElement.query(
+          By.css('[data-testid="scope-input"]')
+        );
+        setInputValue(fixture, input.nativeElement, 'email offline');
+        input = fixture.debugElement.query(
+          By.css('[data-testid="audience-input"]')
+        );
+        setInputValue(fixture, input.nativeElement, 'people');
+        component.authConfig = 'ping';
+        component.webAuthFlow = 'implicit';
+        const authentication = TestBed.inject(AuthenticationService);
+        click(fixture, button.nativeElement);
+        expect(authentication.setBaseConfig).toHaveBeenCalledTimes(1);
+        expect(authentication.setBaseConfig).toHaveBeenCalledWith({
+          authConfig: 'ping',
+          clientID: '1994-9940fks',
+          discoveryUrl: 'https://foo.bar.disco/.well-known/sticky-buns',
+          scope: 'email offline',
+          audience: 'people',
+          webAuthFlow: 'implicit',
+        });
+      }));
+    });
+
+    describe('client ID', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="client-id-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.clientID);
+      });
+
+      it('is enabled', () => {
+        expect(input.nativeElement.disabled).toEqual(false);
+      });
+    });
+
+    describe('discovery URL', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="discovery-url-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.discoveryUrl);
+      });
+
+      it('is enabled', () => {
+        expect(input.nativeElement.disabled).toEqual(false);
+      });
+    });
+
+    describe('scope', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="scope-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.scope);
+      });
+
+      it('is enabled', () => {
+        expect(input.nativeElement.disabled).toEqual(false);
+      });
+    });
+
+    describe('audience', () => {
+      let input: DebugElement;
+      beforeEach(() => {
+        input = fixture.debugElement.query(
+          By.css('[data-testid="audience-input"]')
+        );
+      });
+
+      it('is initialized', async () => {
+        await component.ionViewDidEnter();
+        expect(input.nativeElement.value).toEqual(auth0Config.audience);
+      });
+
+      it('is enabled', () => {
+        expect(input.nativeElement.disabled).toEqual(false);
+      });
     });
   });
 });
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -14,26 +14,74 @@ import {
 })
 export class SettingsPage {
   disableEdits: boolean;
+  oidcServers = [
+    { key: 'auth0', value: 'Auth0' },
+    { key: 'azure', value: 'Azure B2C' },
+    { key: 'cognito', value: 'Cognito (AWS)' },
+    { key: 'identity-server', value: 'Identity Server' },
+    { key: 'keycloak', value: 'Keycloak' },
+    { key: 'okta', value: 'Okta' },
+    { key: 'ping', value: 'Ping' },
+    { key: 'salesforce', value: 'Salesforce' },
+    { key: 'onelogin', value: 'OneLogin' },
+    { key: 'general', value: 'General' },
+  ];
+  authFlows = [
+    { key: 'implicit', value: 'Implicit' },
+    { key: 'PKCE', value: 'PKCE' },
+  ];
+  authConfig: string;
+  clientID: string;
+  discoveryURL: string;
+  audience: string;
+  scope: string;
+  webAuthFlow: string;
 
   constructor(private authentication: AuthenticationService) {}
 
   async ionViewDidEnter() {
     this.disableEdits = await this.authentication.isAuthenticated();
+    return this.initCustomizableFields();
   }
 
-  useAzure(): Promise<void> {
-    return this.authentication.setBaseConfig(azureConfig);
+  async useAzure(): Promise<void> {
+    await this.authentication.setBaseConfig(azureConfig);
+    return this.initCustomizableFields();
   }
 
-  useAWS(): Promise<void> {
-    return this.authentication.setBaseConfig(awsConfig);
+  async useAWS(): Promise<void> {
+    await this.authentication.setBaseConfig(awsConfig);
+    return this.initCustomizableFields();
   }
 
-  useAuth0(): Promise<void> {
-    return this.authentication.setBaseConfig(auth0Config);
+  async useAuth0(): Promise<void> {
+    await this.authentication.setBaseConfig(auth0Config);
+    return this.initCustomizableFields();
   }
 
-  useOkta(): Promise<void> {
-    return this.authentication.setBaseConfig(oktaConfig);
+  async useOkta(): Promise<void> {
+    await this.authentication.setBaseConfig(oktaConfig);
+    return this.initCustomizableFields();
+  }
+
+  async useCustomization(): Promise<void> {
+    return this.authentication.setBaseConfig({
+      authConfig: this.authConfig,
+      clientID: this.clientID,
+      discoveryUrl: this.discoveryURL,
+      scope: this.scope,
+      audience: this.audience,
+      webAuthFlow: this.webAuthFlow,
+    });
+  }
+
+  private async initCustomizableFields(): Promise<void> {
+    const config = await this.authentication.getBaseConfig();
+    this.authConfig = config.authConfig;
+    this.clientID = config.clientID;
+    this.discoveryURL = config.discoveryUrl;
+    this.scope = config.scope;
+    this.audience = config.audience;
+    this.webAuthFlow = config.webAuthFlow;
   }
 }
